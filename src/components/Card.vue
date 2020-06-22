0ebb27bd48f9931dbb661549fe014b48d979e582
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2020-06-22 04:15:04 +0300

    Added About page

---
+++ src/components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-card
+      max-width="344"
+      class="mx-auto"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="headline title-center"> {{name}} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-layout
+        align-center
+        justify-space-around
+        wrap
+      >
+      <v-avatar size="200">
+        <v-img
+          :src="require('@/assets/' + imageName + '')"
+          class="img-profile"
+        ></v-img>
+      </v-avatar>
+      </v-layout>
+      <v-card-text>
+        {{description}}
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn icon :href="github">
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+        <v-btn icon :href="linkedin">
+          <v-icon>mdi-linkedin</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+  props: ['name', 'imageName', 'description', 'github', 'linkedin'],
+  data() {
+    return {};
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+<style scoped>
+.title-center{
+    justify-content: center;
+    flex-direction: row;
+    text-align: center;
+    align-items: center;
+}
+</style>
--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,15 +1,62 @@
 <template>
     <v-container>
-        <h2>About Team page</h2>
+    <v-header>About our Team</v-header>
+    <v-divider/>
+
+    <div class="card-container">
+      <Card v-for="item in items"
+                :key="item.title"
+                :name="item.name"
+                :imageName="item.imageName"
+                :description="item.description"
+                :github="item.github"
+                :linkedin="item.linkedin" />
+    </div>
     </v-container>
 </template>
 
 <script>
+import Card from '../Card.vue';
+
 export default {
+  components: {
+    Card,
+  },
   data() {
-    return {};
+    return {
+      items: [{
+        name: 'Nikolas Bompetsis',
+        imageName: 'nbob.jpeg',
+        description: 'Super nerd who loves Vim, Linux/OS X, new technologies and to be involved in Open-Source projects.',
+        github: 'https://github.com/nbompetsis',
+        linkedin: 'https://linkedin.com/in/nikolas-bompetsis-b883b869',
+      }, {
+        name: 'Nikolas Bompetsis',
+        imageName: 'nbob.jpeg',
+        description: 'Super nerd who loves Vim, Linux/OS X, new technologies and to be involved in Open-Source projects.',
+        github: 'https://github.com/nbompetsis',
+        linkedin: 'https://linkedin.com/in/nikolas-bompetsis-b883b869',
+      }, {
+        name: 'Nikolas Bompetsis',
+        imageName: 'nbob.jpeg',
+        description: 'Super nerd who loves Vim, Linux/OS X, new technologies and to be involved in Open-Source projects.',
+        github: 'https://github.com/nbompetsis',
+        linkedin: 'https://linkedin.com/in/nikolas-bompetsis-b883b869',
+      },
+      ],
+    };
   },
   mounted() {
   },
 };
 </script>
+<style scoped>
+.card-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-flow: center;
+  margin: 20px 20px 0px 0px;
+  padding-bottom: 10px;
+}
+</style>
