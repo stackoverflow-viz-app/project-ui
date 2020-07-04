bdb6c107e5916efea0d430053c8c14cef6b59880
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2020-07-04 22:30:45 +0300

    Added network graph chart

--- src/components/pages/Dashboards.vue
+++ src/components/pages/Dashboards.vue
@@ -1,12 +1,18 @@
 <template>
-    <v-container>
-      <v-toolbar-title>Dashboards</v-toolbar-title>
-      <v-divider/>
-    </v-container>
+  <div>
+  <v-toolbar-title>Dashboards</v-toolbar-title>
+  <v-divider/>
+  <NetworkGraph width="900" height="600"/>
+  </div>
 </template>
 
 <script>
+import NetworkGraph from '../diagrams/NetworkGraph.vue';
+
 export default {
+  components: {
+    NetworkGraph,
+  },
   data() {
     return {};
   },
